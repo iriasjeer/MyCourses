39929b31c8f2f1d994e5c1981a922c367ee44b67
iriasjeer <jeerly@gmail.com>
2016-04-02 09:27:14 -0600

    Added files via upload

---
+++ Site/js/me.js
@@ -0,0 +1,24 @@
+    $(window).resize(function(){
+    $("#pic").attr("src","images/Me1.jpg");
+    $("#change").html("Master in Business Manager and Marketing student. I'm a web design teacher.");
+  })  
+
+  $("#mom").click(function ()
+  {
+    $("#pic").attr("src","images/Me.jpg");
+    $("#change").html("Master in Business Manager and Marketing student. I'm a web design teacher.");
+  });
+
+  $("#til").click(function ()
+  {
+
+    $("#pic").attr("src","images/ThingsILike.jpg");
+    $("#change").html("Things I Like: Series: The Flash / Food: Shrimps, Flan, Cheesecake / Drinks: Kiwi Juice / Futball Team: FC Barcelona / Sports: Run, VolleyBall / Books: Looking For Alaska, The TimeKeeper / Singers: Black Eyed Peas, Bruno Mars, Ana Torroja, Alejandro Sanz, Taylor Swift / Movies:My Sisters Keeper, TGDT, X-Men / Other: Dance");
+  });
+
+  $("#mc").click(function ()
+  {
+
+    $("#pic").attr("src","images/MyCountry.jpg");
+    $("#change").html("Honduras is a wonderfull country. I like to travel. Some magic places in Honduras are: Beaches: Tela, Omoa, Trujillo, Amapala / Colonial Cities: Comayagua, Santa Lucia, La Esperanza, Other: Battle of San Fernando of Omoa, Jeanette Kawas National Park / Most Popular Places: the majestically Copan Ruins and the Coral Reef in Cayos Cochinos and Bay Island. Honduras we are for you!");
+  });   
